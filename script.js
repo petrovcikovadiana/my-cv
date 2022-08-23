ac89e620cad120b5ea34e674cea7d01634d8f682
Diana Petrovčíková <63262388+petrovcikovadiana@users.noreply.github.com>
2022-08-23 19:46:54 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -29,28 +29,28 @@ document.addEventListener("keydown", function (e) {
   }
 });
 
-// Get the modal
-var modals = document.getElementById("myModal");
+// // Get the modal
+// var modals = document.getElementById("myModal");
 
-// Get the button that opens the modal
-var btn = document.getElementById("myBtn");
+// // Get the button that opens the modal
+// var btn = document.getElementById("myBtn");
 
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("closes")[0];
+// // Get the <span> element that closes the modal
+// var span = document.getElementsByClassName("closes")[0];
 
-// When the user clicks on the button, open the modal
-btn.onclick = function () {
-  modals.style.display = "block";
-};
+// // When the user clicks on the button, open the modal
+// btn.onclick = function () {
+//   modals.style.display = "block";
+// };
 
-// When the user clicks on <span> (x), close the modal
-span.onclick = function () {
-  modals.style.display = "none";
-};
+// // When the user clicks on <span> (x), close the modal
+// span.onclick = function () {
+//   modals.style.display = "none";
+// };
 
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function (event) {
-  if (event.target == modals) {
-    modals.style.display = "none";
-  }
-};
+// // When the user clicks anywhere outside of the modal, close it
+// window.onclick = function (event) {
+//   if (event.target == modals) {
+//     modals.style.display = "none";
+//   }
+// };
